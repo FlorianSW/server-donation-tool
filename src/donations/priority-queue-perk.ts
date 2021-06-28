7735d080282fb218f9674433f5b6bd9d194ed7d6
Florian <florian.schmidt.welzow@t-online.de>
2021-06-28 21:02:59 +0200

    Renew priority queue if existing entry expires before new one would

--- src/donations/priority-queue-perk.ts
+++ src/donations/priority-queue-perk.ts
@@ -12,32 +12,64 @@ export class PriorityQueuePerk implements RedeemPerk {
     }
 
     async redeem(p: Package, perk: Perk, id: Identifier, order: Order): Promise<TranslateParams> {
-        const orderTime = new Date(order.create_time);
-        const expiration = new Date(orderTime.valueOf());
-        expiration.setDate(orderTime.getDate() + perk.amountInDays);
+        const steamId = SteamId64.of(id.steamId)
 
         const successParams: TranslateParams = ['PRIORITY_QUEUE_REDEEM_COMPLETE', {
             params: {
-                until: expiration.toLocaleString(),
+                until: this.expiration(order, perk).toLocaleString(),
                 serverName: this.serverNames[perk.cftools.serverApiId],
             }
         }];
         try {
-            await this.cftools.putPriorityQueue({
-                serverApiId: ServerApiId.of(perk.cftools.serverApiId),
-                id: SteamId64.of(id.steamId),
-                expires: expiration,
-                comment: `Created by CFTools Server Donation bot.
-PayPal Transaction ID: ${order.purchase_units[0]?.payments?.captures[0]?.id}
-PayPal Order ID: ${order.id}
-Selected product: ${p.name}`
-            });
+            await this.createPriority(steamId, perk, p, order);
         } catch (e) {
             if (e instanceof DuplicateResourceCreation) {
+                await this.replacePriorityIfOlder(steamId, perk, p, order);
                 return successParams;
             }
             throw e;
         }
         return successParams;
+    }
+
+    private async replacePriorityIfOlder(steamId: SteamId64, perk: Perk, p: Package, order: Order): Promise<void> {
+        const newExpiration = this.expiration(order, perk);
+        const request = {
+            serverApiId: ServerApiId.of(perk.cftools.serverApiId),
+            playerId: steamId,
+        };
+        const item = await this.cftools.getPriorityQueue(request);
+        if (!item) {
+            return await this.createPriority(steamId, perk, p, order);
+        }
+        if (item.expiration === 'Permanent') {
+            return;
+        }
+        if (item.expiration.getTime() > newExpiration.getTime()) {
+            return;
+        }
+        await this.cftools.deletePriorityQueue(request);
+
+        return await this.createPriority(steamId, perk, p, order);
+    }
+
+    private expiration(order: Order, perk: Perk): Date {
+        const orderTime = new Date(order.create_time);
+        const expiration = new Date(orderTime.valueOf());
+        expiration.setDate(orderTime.getDate() + perk.amountInDays);
+
+        return expiration;
+    }
+
+    private async createPriority(steamId: SteamId64, perk: Perk, p: Package, order: Order): Promise<void> {
+        await this.cftools.putPriorityQueue({
+            serverApiId: ServerApiId.of(perk.cftools.serverApiId),
+            id: steamId,
+            expires: this.expiration(order, perk),
+            comment: `Created by CFTools Server Donation bot.
+PayPal Transaction ID: ${order.purchase_units[0]?.payments?.captures[0]?.id}
+PayPal Order ID: ${order.id}
+Selected product: ${p.name}`
+        });
     }
 }
