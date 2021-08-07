61b29f365fc9269161b3c43c9804bf6459d1259d
Florian <florian.schmidt.welzow@t-online.de>
2021-08-07 15:53:12 +0200

    refactoring

--- src/adapter/controller/statistics.ts
+++ src/adapter/controller/statistics.ts
@@ -1,55 +1,34 @@
 import {inject, singleton} from 'tsyringe';
 import {Request, Response, Router} from 'express';
 import {requireAuthentication} from '../../auth';
-import {OrderRepository} from '../../domain/repositories';
-import {AppConfig} from '../../domain/app-config';
 import {translate} from '../../translations';
-
-function currencyMapping(currency: string) {
-    switch (currency) {
-        case 'USD':
-            return '$';
-        case 'EUR':
-        case 'EURO':
-            return '€';
-        default:
-            return currency;
-    }
-}
+import {CalculateDonationTarget} from '../../service/donation-target';
 
 @singleton()
 export class StatisticsController {
     public readonly router: Router = Router();
-    private readonly monthlyTarget: number;
 
-    constructor(
-        @inject('OrderRepository') private readonly repository: OrderRepository,
-        @inject('AppConfig') config: AppConfig,
-    ) {
-        if (config.app.community?.donationTarget?.monthly) {
-            this.monthlyTarget = config.app.community.donationTarget.monthly;
+    constructor(@inject(CalculateDonationTarget) private readonly service: CalculateDonationTarget) {
+        if (service.hasMonthlyTarget()) {
             this.router.get('/api/statistics/monthly', requireAuthentication, this.monthlyStatistics.bind(this));
         }
     }
 
     private async monthlyStatistics(req: Request, res: Response): Promise<void> {
-        const now = new Date();
-        const startOfMonth = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), 1, 0, 0, 0));
-        const orders = await this.repository.findCreatedAfter(startOfMonth);
-        const totalDonations = orders.map((o) => parseFloat(o.reference.p.price.amount)).reduce((pv, cv) => pv + cv);
+        const target = await this.service.monthly();
         let msgKey = 'DONATION_TARGET_CLAIM';
-        if (totalDonations >= this.monthlyTarget) {
+        if (target.totalAmount >= target.target) {
             msgKey = 'DONATION_TARGET_REACHED';
         }
         res.send({
-            currency: currencyMapping(orders[0].reference.p.price.currency),
-            totalAmount: totalDonations,
-            target: this.monthlyTarget,
+            currency: target.currency,
+            totalAmount: target.totalAmount,
+            target: target.target,
             message: translate(msgKey, {
                 params: {
-                    totalAmount: totalDonations.toString(10),
-                    currency: currencyMapping(orders[0].reference.p.price.currency),
-                    target: this.monthlyTarget.toString(10),
+                    totalAmount: target.totalAmount.toString(10),
+                    currency: target.currency,
+                    target: target.target.toString(10),
                 }
             }),
         });
---
+++ src/service/donation-target.ts
@@ -0,0 +1,47 @@
+import {inject, singleton} from 'tsyringe';
+import {OrderRepository} from '../domain/repositories';
+import {AppConfig} from '../domain/app-config';
+
+export interface DonationTarget {
+    currency: string;
+    totalAmount: number;
+    target: number;
+}
+
+function currencyMapping(currency: string) {
+    switch (currency) {
+        case 'USD':
+            return '$';
+        case 'EUR':
+        case 'EURO':
+            return '€';
+        default:
+            return currency;
+    }
+}
+
+@singleton()
+export class CalculateDonationTarget {
+    private readonly monthlyTarget: number;
+
+    constructor(@inject('OrderRepository') private readonly repository: OrderRepository, @inject('AppConfig') config: AppConfig) {
+        this.monthlyTarget = config.app.community.donationTarget.monthly;
+    }
+
+    hasMonthlyTarget(): boolean {
+        return this.monthlyTarget !== undefined;
+    }
+
+    async monthly(): Promise<DonationTarget> {
+        const now = new Date();
+        const startOfMonth = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), 1, 0, 0, 0));
+        const orders = await this.repository.findCreatedAfter(startOfMonth);
+        const totalDonations = orders.map((o) => parseFloat(o.reference.p.price.amount)).reduce((pv, cv) => pv + cv);
+
+        return {
+            currency: currencyMapping(orders[0].reference.p.price.currency),
+            target: this.monthlyTarget,
+            totalAmount: totalDonations,
+        };
+    }
+}
